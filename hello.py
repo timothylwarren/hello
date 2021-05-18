5c652b225ca042736947a3e8f1e41d2fbf4b8217
timothylwarren <timlwarren@gmail.com>
2021-05-18 13:41:37 -0700

    removed third line

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
 def saying_hello():
     print('Just wanted to say hello to my collaborator')
-    print('Owner says hola')
-    print('Owner and Collaborator write third line')
-    
+    print('Owner says hola')   
